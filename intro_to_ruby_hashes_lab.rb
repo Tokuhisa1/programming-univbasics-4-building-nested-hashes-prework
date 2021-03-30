1dcd504a72acfb94b64a3aaf4b94bc3f3048a911
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-03-30 16:57:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,7 +51,7 @@ def monopoly_with_fourth_tier
   hash[:reading_railroad][:mortgage_value]      = 100
   hash[:pennsylvania_railroad][:mortgage_value] = 200
   hash[:b_and_o_railroad][:mortgage_value]      = 400
-  hash[:shortline_railroad][:mortgage_value]    = 100
+  hash[:shortline_railroad][:mortgage_value]    = 800
 
   hash
 end
